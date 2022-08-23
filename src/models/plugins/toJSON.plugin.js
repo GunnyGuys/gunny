29d591a85fa70652ba561031b8eeeecfec9b96e0
Thinh Nguyen Hoang <blv.tu97@gmail.com>
2022-08-23 21:16:14 +0700

    delete private nested objects

--- src/models/plugins/toJSON.plugin.js
+++ src/models/plugins/toJSON.plugin.js
@@ -3,6 +3,16 @@
  *  - removes __v, createdAt, updatedAt, and any path that has private: true
  *  - replaces _id with id
  */
+
+const deleteAtPath = (obj, path, index) => {
+  // end of path
+  if (index === path.length - 1) {
+    delete obj[path[index]];
+    return;
+  }
+  deleteAtPath(obj[path[index]], path, index + 1);
+};
+
 const toJSON = (schema) => {
   let transform;
   if (schema.options.toJSON && schema.options.toJSON.transform) {
@@ -13,7 +23,8 @@ const toJSON = (schema) => {
     transform(doc, ret, options) {
       Object.keys(schema.paths).forEach((path) => {
         if (schema.paths[path].options && schema.paths[path].options.private) {
-          delete ret[path];
+          // delete ret[path];
+          deleteAtPath(ret, path.split("."), 0);
         }
       });
 
@@ -30,3 +41,45 @@ const toJSON = (schema) => {
 };
 
 module.exports = toJSON;
+
+// Subdocument
+// const userSchema = new Schema({
+//   child: new mongoose.Schema({
+//     name: {
+//       type: String,
+//       required: true,
+//       private: true
+//     },
+//     age:{
+//       type: Number,
+//       private: true
+//     },
+//   })
+// });
+
+// Nested Paths
+// const userSchema = new Schema({
+//     child: {
+//       name: {
+//         type: String,
+//         required: true,
+//         private: true
+//       },
+//       age:{
+//         type: Number,
+//         private: true
+//       },
+//   }
+// });
+
+// const User = mongoose.model('User', userSchema);
+// const Parent = mongoose.model('Parent', parentSchema);
+
+// const deleteAtPath = (obj, path, index) => {
+//     if (index === path.length - 1) {
+//       const key = path[index]
+//       delete obj[key];
+//       return;
+//     }
+//     deleteAtPath(obj[path[index]], path, index + 1);
+//   };
