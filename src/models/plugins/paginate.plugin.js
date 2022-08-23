6e258907a7fa3b0448c8e8a724a9b573ff739814
Thinh Nguyen Hoang <blv.tu97@gmail.com>
2022-08-23 21:15:16 +0700

    Added support for populate data

--- src/models/plugins/paginate.plugin.js
+++ src/models/plugins/paginate.plugin.js
@@ -11,6 +11,7 @@ const paginate = (schema) => {
    * Query for documents with pagination
    * @param {Object} [filter] - Mongo filter
    * @param {Object} [options] - Query options
+   * @param {string} [options.populate] - populate data fields. Hierarchy of fields should be separated by (.). Multiple sorting criteria should be separated by commas (,)
    * @param {string} [options.sortBy] - Sorting criteria using the format: sortField:(desc|asc). Multiple sorting criteria should be separated by commas (,)
    * @param {number} [options.limit] - Maximum number of results per page (default = 10)
    * @param {number} [options.page] - Current page (default = 1)
@@ -39,11 +40,36 @@ const paginate = (schema) => {
     const skip = (page - 1) * limit;
 
     const countPromise = this.countDocuments(filters).exec();
-    const docsPromise = this.find(filters)
-      .sort(sort)
-      .skip(skip)
-      .limit(limit)
-      .exec();
+    const docsPromise = this.find(filters).sort(sort).skip(skip).limit(limit);
+
+    /**
+     * format { path: param1, populate: param2 } using
+     * when need a nested populate, for example
+     * Student:
+     * {
+     *    name: "Student1",
+     *    age: 19,
+     *    course:{
+     *      lesson: [Array],
+     *       name: 'Course1',
+     *       teacher: '5eeccbrfdbghejkrdhjk3466',
+     *    }
+     * }
+     * Student.find()
+     * .populate('course)
+     * .populate({path: 'course, populate: 'teacher' })
+     */
+    if (options.populate) {
+      options.populate.split(",").forEach((populateOption) => {
+        docsPromise = docsPromise.populate(
+          populateOption
+            .split(".")
+            .reverse()
+            .reduce((a, b) => ({ path: b, populate: a }))
+        );
+      });
+    }
+    docsPromise = docsPromise.exec();
 
     return Promise.all([countPromise, docsPromise]).then((values) => {
       const [totalResults, results] = values;
@@ -61,3 +87,28 @@ const paginate = (schema) => {
 };
 
 module.exports = paginate;
+
+// School:
+//  {
+//     name: "SPKT",
+//     address: 'Vo Van Ngan',
+//     class:{
+//        name: 'classA',
+//        student: '5eeccbrfdbghejkrdhjk3466',
+//     }
+//  }
+
+// const array1 = ['School','Class', 'Student'];
+// const result = array1.reverse().reduce(
+//   (a, b) =>{
+//     console.log(a);
+//     console.log(b);
+//     return { 'path': b, 'populate': a }
+//   }
+// );
+
+// > "Student"
+// > "Class"
+// > Object { path: "Class", populate: "Student" }
+// > "School"
+// > Object { path: "School", populate: Object { path: "Class", populate: "Student" } }
